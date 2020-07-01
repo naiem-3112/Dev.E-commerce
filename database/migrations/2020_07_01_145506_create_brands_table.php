41c1641b82063134514a61376b9d411a62c8289f
ShahadatPOM <naiem.3112@gmail.com>
2020-07-01 20:57:08 +0600

    brand model, controller, migration created

--- database/migrations/2020_07_01_145506_create_brands_table.php
+++ database/migrations/2020_07_01_145506_create_brands_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCategoriesTable extends Migration
+class CreateBrandsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,8 @@ class CreateCategoriesTable extends Migration
      */
     public function up()
     {
-        Schema::create('categories', function (Blueprint $table) {
+        Schema::create('brands', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->unique();
-            $table->string('slug');
-            $table->text('description')->nullable();
-            $table->string('image')->nullable();
-            $table->boolean('status');
             $table->timestamps();
         });
     }
@@ -31,6 +26,6 @@ class CreateCategoriesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('categories');
+        Schema::dropIfExists('brands');
     }
 }
